e1a9715aca15015b2f73444929d0c8211a2332b4
xuzewei <microprograms@126.com>
2019-12-01 12:57:52 +0800

    9.13.0

---
+++ src/main/java/com/github/microprograms/maven_micro_api_sdk_plugin/UpdateShowDoc.java
@@ -0,0 +1,53 @@
+package com.github.microprograms.maven_micro_api_sdk_plugin;
+
+import java.io.File;
+
+import com.github.microprograms.micro_api_sdk.model.ModuleDefinition;
+import com.github.microprograms.micro_api_sdk.model.ShowdocDefinition;
+import com.github.microprograms.micro_api_sdk.utils.ApiSdk;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "update-show-doc", defaultPhase = LifecyclePhase.COMPILE)
+public class UpdateShowDoc extends AbstractMojo {
+
+	@Parameter(defaultValue = "src/main/resources/api.json")
+	private String apiConfigFilePath;
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		getLog().info("------------------------------------------------------------------------");
+		getLog().info("micro-api-sdk: update-show-doc");
+		getLog().info("------------------------------------------------------------------------");
+		try {
+			if (!new File(apiConfigFilePath).exists()) {
+				getLog().info(String.format("%s not exists, nothing to update.", apiConfigFilePath));
+				return;
+			}
+
+			ModuleDefinition moduleDefinition = ApiSdk.build(apiConfigFilePath);
+			ShowdocDefinition showdocDefinition = moduleDefinition.getShowdocDefinition();
+			if (null == showdocDefinition) {
+				getLog().info(String.format("showdocDefinition not exists, nothing to update."));
+				return;
+			}
+
+			if (StringUtils.isBlank(showdocDefinition.getApiKey())
+					|| StringUtils.isBlank(showdocDefinition.getApiToken())) {
+				getLog().info(String.format("apiKey or apiToken not exists, nothing to update."));
+				return;
+			}
+
+			ApiSdk.ShowDoc.update(moduleDefinition);
+		} catch (Exception e) {
+			throw new MojoFailureException("", e);
+		}
+	}
+
+}
